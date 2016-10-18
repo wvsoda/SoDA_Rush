a8245f35a2dd12d87d0b591759ae1c43f6d9dfc3
AgentG010 <georgetroulis@gmail.com>
2016-10-17 17:20:26 -0700

    Added stuff in Entity

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,5 +3,20 @@ package entity;
 
 public abstract class Entity
 {
+    protected int xCoord;
+    protected int yCoord;
+    protected double angle;
 
+    public Entity(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+        angle = 0.0;
+    }
+
+    public void move(int newX, int newY)
+    {
+        xCoord = newX;
+        yCoord = newY;
+    }
 }
